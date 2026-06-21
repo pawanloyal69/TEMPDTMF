9263c0179e8b8a69112ed5c94f0539719ff6736d
pawanloyal69 <pawanloyal69@gmail.com>
2026-06-21 13:35:39 +0530

    Update voice.js

--- api/voice.js
+++ api/voice.js
@@ -1,1 +1,15 @@
+module.exports = async (req, res) => {
+  res.setHeader("Content-Type", "text/xml");
 
+  const twiml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Gather numDigits="1"
+          action="/api/gather"
+          method="POST">
+    <Say>Please press 1 to continue.</Say>
+  </Gather>
+  <Say>No input received. Goodbye.</Say>
+</Response>`;
+
+  res.status(200).send(twiml);
+};
