17ef85f45ec251b8e7317c6aa8c99dc17d2d4af6
pawanloyal69 <pawanloyal69@gmail.com>
2026-06-21 13:36:27 +0530

    Update gather.js

--- api/gather.js
+++ api/gather.js
@@ -1,1 +1,19 @@
+module.exports = async (req, res) => {
+  let digit = "No digit received";
 
+  if (req.body && req.body.Digits) {
+    digit = req.body.Digits;
+  }
+
+  console.log("DTMF Pressed:", digit);
+
+  res.setHeader("Content-Type", "text/xml");
+
+  const twiml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Say>Thank you. Your input has been received. Goodbye.</Say>
+  <Hangup/>
+</Response>`;
+
+  res.status(200).send(twiml);
+};
